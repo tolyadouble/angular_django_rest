3cdc9934cf9688eacb39c2673ca8e1224b7bbc61
Tolya Double <tolya.double@gmail.com>
2016-05-30 11:08:19 +0300

    change settings CORS place

--- settings.py
+++ settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
+CORS_ORIGIN_ALLOW_ALL = True
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
