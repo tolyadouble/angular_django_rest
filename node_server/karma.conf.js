c9717b0feb81feb9b9394510bba8e797bf2dcc4a
Anatoliy <tolya.double@gmail.com>
2016-06-07 16:43:47 +0300

    code style changes

--- node_server/karma.conf.js
+++ node_server/karma.conf.js
@@ -3,70 +3,70 @@
 
 module.exports = function(config) {
   config.set({
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
+    plugins: [
+      'karma-mocha',
+      'karma-coverage',
+      'karma-phantomjs-launcher',
+      'karma-chai'
+    ],
 
-  // base path that will be used to resolve all patterns (eg. files, exclude)
-  basePath: '',
-  plugins: [
-    'karma-mocha',
-    'karma-coverage',
-    'karma-phantomjs-launcher',
-    'karma-chai'
-  ],
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['mocha', 'chai'],
 
-  // frameworks to use
-  // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-  frameworks: ['mocha', 'chai'],
+    // list of files / patterns to load in the browser
+    files: [
+      'node_modules/angular/angular.js',
+      'node_modules/angular-mocks/angular-mocks.js',
+      'node_modules/angular-route/angular-route.js',
+      'node_modules/ngstorage/ngStorage.min.js',
+      'public/js/*.js',
+      'tests/*.js'
+    ],
 
-  // list of files / patterns to load in the browser
-  files: [
-    'node_modules/angular/angular.js',
-    'node_modules/angular-mocks/angular-mocks.js',
-    'node_modules/angular-route/angular-route.js',
-    'node_modules/ngstorage/ngStorage.min.js',
-    'public/js/*.js',
-    'tests/*.js'
-  ],
+    // list of files to exclude
+    exclude: [
+    ],
 
-  // list of files to exclude
-  exclude: [
-  ],
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+      'public/js/*.js': ['coverage']
+    },
 
-  // preprocess matching files before serving them to the browser
-  // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-  preprocessors: {
-    'public/js/*.js': ['coverage']
-  },
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress', 'coverage'],
 
-  // test results reporter to use
-  // possible values: 'dots', 'progress'
-  // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-  reporters: ['progress', 'coverage'],
+    coverageReporter: {
+      type : 'html',
+      dir : 'coverage/'
+    },
 
-  coverageReporter: {
-    type : 'html',
-    dir : 'coverage/'
-  },
+    // web server
+    hostname: '127.0.0.1',
+    port: 8003,
 
-  // web server
-  hostname: '127.0.0.1',
-  port: 8003,
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
 
-  // enable / disable colors in the output (reporters and logs)
-  colors: true,
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
 
-  // level of logging
-  // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-  logLevel: config.LOG_INFO,
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
 
-  // enable / disable watching file and executing tests whenever any file changes
-  autoWatch: true,
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['PhantomJS'],
 
-  // start these browsers
-  // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-  browsers: ['PhantomJS'],
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: false
     
-  // Continuous Integration mode
-  // if true, Karma captures browsers, runs the tests and exits
-  singleRun: false
   });
 };
